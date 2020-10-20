873c0537c36b196f62f56b8f461581b8a9cd771f
ibalram <balramsinghrathore3@gmail.com>
2020-10-20 08:33:37 +0530

    Added: Master branch

---
+++ src/main/java/com/cg/censusanalyser/App.java
@@ -0,0 +1,7 @@
+package com.cg.censusanalyser;
+
+public class App {
+	public static void main(String[] args) {
+		System.out.println("Indian States Census Analyser");
+	}
+}
---
+++ src/test/java/com/cg/censusanalyser/AppTest.java
@@ -0,0 +1,12 @@
+package com.cg.censusanalyser;
+
+import static org.junit.Assert.assertTrue;
+import org.junit.*;
+
+public class AppTest {
+
+	@Test
+	public void testApp() {
+		assertTrue(true);
+	}
+}
