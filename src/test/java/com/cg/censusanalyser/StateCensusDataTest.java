9160121f863b70dd86bf685ab820d6acf2301d41
ibalram <balramsinghrathore3@gmail.com>
2020-10-22 10:11:57 +0530

    Added: Test to sort the given census data by state

--- src/test/java/com/cg/censusanalyser/StateCensusDataTest.java
+++ src/test/java/com/cg/censusanalyser/StateCensusDataTest.java
@@ -3,6 +3,9 @@ package com.cg.censusanalyser;
 import static org.junit.Assert.assertEquals;
 import org.junit.*;
 
+import com.google.gson.Gson;
+import com.google.gson.JsonSyntaxException;
+
 public class StateCensusDataTest {
 	public static final String STATE_CENSUS_FILE = "./IndiaStateCensusData.csv";
 	public static final String STATE_CENSUS_INCORECT_DELIMITER_FILE = "./WrongStateCensusData.csv";
@@ -35,7 +38,7 @@ public class StateCensusDataTest {
 			assertEquals(StateCensusAnalyserException.ExceptionType.INCORRECT_CSV, e.type);
 		}
 	}
-	
+
 	@Test
 	public void givenStateCensusIncorectDelimiterShouldThrowException() {
 		try {
@@ -53,4 +56,19 @@ public class StateCensusDataTest {
 			assertEquals(StateCensusAnalyserException.ExceptionType.RUNTIME_EXCEPTION, e.type);
 		}
 	}
+
+	@Test
+	public void givenStateCensusDataWhenSortedOnStateShouldReturnSorted() {
+		try {
+			censusAnalyser.loadStateCsvData(STATE_CENSUS_FILE);
+			String sortedCensusData = censusAnalyser.getStateWiseSortdCensusData(STATE_CENSUS_FILE);
+			StateCensus[] censusCSV = new Gson().fromJson(sortedCensusData, StateCensus[].class);
+			assertEquals("Andhra Pradesh", censusCSV[0].state);
+		} catch (JsonSyntaxException e) {
+			e.printStackTrace();
+		} catch (StateCensusAnalyserException e) {
+			e.printStackTrace();
+		}
+	}
+
 }
